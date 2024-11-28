091769a1ba433f2afc929b387b233b6449650aee
Simon Schmadlbauer <sschmadl@htl-steyr.ac.at>
2024-11-28 09:33:28 +0100

    added launch()

--- src/main/java/htl/steyr/adventcalender/AdventCalender.java
+++ src/main/java/htl/steyr/adventcalender/AdventCalender.java
@@ -91,4 +91,8 @@ public class AdventCalender extends Application {
         imageStage.setScene(imageScene);
         imageStage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
