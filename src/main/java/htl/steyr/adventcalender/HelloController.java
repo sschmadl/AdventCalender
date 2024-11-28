c54a76fe62172a983cf772019929f596ba1b7c98
Simon Schmadlbauer <sschmadl@htl-steyr.ac.at>
2024-11-28 10:40:11 +0100

    changed pom.xml

--- src/main/java/htl/steyr/adventcalender/HelloController.java
+++ src/main/java/htl/steyr/adventcalender/HelloController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+//
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
